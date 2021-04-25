18304d5bd2a4c5ba6ca5d83ba8d67eb2066ed8b8
Bram Bonné <bram.bonne@gmail.com>
2021-04-25 13:10:11 +0200

    Remove C-based emoji support

--- keyboards/annepro2/keymaps/rusty/keymap.c
+++ keyboards/annepro2/keymaps/rusty/keymap.c
@@ -15,56 +15,6 @@ void process_keypress(uint16_t);
 void layer_changed(uint8_t);
 void caps_lock_changed(bool);
 
-enum anne_pro_layers {
-  _BASE_LAYER,
-  _MOVEMENT_LAYER,
-  _FN2_LAYER,
-  _NUMPAD_LAYER,
-};
-
-enum custom_keycodes {
-  TMUX_DOWN = AP2_SAFE_RANGE,
-  TMUX_UP,
-  TMUX_LEFT,
-  TMUX_RIGHT
-};
-
-enum unicode_names {
-    LAUGH,
-    SWEAT,
-    SLIGHT_SMILE,
-    SMILE_BLUSH,
-    CONFUSED,
-    OPEN_MOUTH,
-    FLUSHED,
-    TONGUE,
-    SAD,
-    THUMBS,
-    WINK,
-    MOUTHLESS,
-    KISS,
-    KISS_HEART,
-    SUNGLASSES
-};
-
-const uint32_t PROGMEM unicode_map[] = {
-    [LAUGH]  = 0x1F604,
-    [SWEAT] = 0x1F605,
-    [SLIGHT_SMILE]  = 0x1F642,
-    [SMILE_BLUSH] = 0x1F60A,
-    [CONFUSED] = 0x1F615,
-    [OPEN_MOUTH] = 0x1F62E,
-    [FLUSHED] = 0x1F633,
-    [TONGUE] = 0x1F61B,
-    [SAD] = 0x1F641,
-    [THUMBS] = 0x1F44D,
-    [WINK] = 0x1F609,
-    [MOUTHLESS] = 0x1F636,
-    [KISS] = 0x1F61A,
-    [KISS_HEART] = 0x1F618,
-    [SUNGLASSES] = 0x1F60E,
-};
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
       process_keypress(keycode);
