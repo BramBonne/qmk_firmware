ac0069ca98bd4dc09b07c1f36b6ca94dfb678d63
Bram Bonné <bram.bonne@gmail.com>
2021-05-19 22:06:04 +0200

    Remove some unnecessary bits

--- keyboards/annepro2/keymaps/rusty/config.h
+++ keyboards/annepro2/keymaps/rusty/config.h
@@ -1,6 +1,3 @@
 // Obins stock firmware has something similar to this already enabled, but disabled by default in QMK
 #define PERMISSIVE_HOLD
 #define TAPPING_TOGGLE 1
-#define UNICODE_SELECTED_MODES UC_LNX, UC_WINC
-#define UNICODE_CYCLE_PERSIST false
-#define UNICODE_SONG_LNX  UNICODE_LINUX
