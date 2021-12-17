6d7b5915c0f6b1a03cdbba581b2c072ccc3467c1
Bram Bonné <bram.bonne@gmail.com>
2021-12-17 18:20:41 +0100

    Fix for keyboard wakeup.

--- keyboards/annepro2/keymaps/rusty/config.h
+++ keyboards/annepro2/keymaps/rusty/config.h
@@ -1,3 +1,5 @@
 // Obins stock firmware has something similar to this already enabled, but disabled by default in QMK
 #define PERMISSIVE_HOLD
 #define TAPPING_TOGGLE 1
+// Prevents keyboard from waking computer, but also makes it work after waking
+#define NO_USB_STARTUP_CHECK 1
